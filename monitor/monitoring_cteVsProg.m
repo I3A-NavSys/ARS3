5aeca749adb46aa30940b40396f6e99b58ea7571
MariaCarmonaP <maria.carmona1@alu.uclm.es>
2022-04-07 15:19:18 +0200

    nuevos .csv con AC5

--- monitor/monitoring_cteVsProg.m
+++ monitor/monitoring_cteVsProg.m
@@ -1,14 +1,14 @@
 %% CARGA DE DATOS
 clear; clc;
 
-mon    = MONITORclass("logAC1.90s.velProg.csv");
-monCte = MONITORclass("logAC1.90s.velCte.csv");
-[posX,posY,posZ,psi,Vhor,Acel]             = mon.ACinfo(1,false);
-[posXCte,posYCte,posZCte,psiCte,VhorCte,~] = monCte.ACinfo(1,false);
-[dist,distXY,distZ]           = mon.dist2ref(1);
-[distCte,distXYCte,distZCte]  = monCte.dist2ref(1);
-[cdist,~,~,closestAC,~]       = mon.dist2ACs(1,30);
-[cdistCte,~,~,closestACCte,~] = monCte.dist2ACs(1,30);
+mon    = MONITORclass("logAC5.90s.velProg.csv");
+monCte = MONITORclass("logAC5.90s.velCte.csv");
+[posX,posY,posZ,psi,Vhor,Acel]             = mon.ACinfo(5,false);
+[posXCte,posYCte,posZCte,psiCte,VhorCte,~] = monCte.ACinfo(5,false);
+[dist,distXY,distZ]           = mon.dist2ref(5);
+[distCte,distXYCte,distZCte]  = monCte.dist2ref(5);
+[cdist,~,~,closestAC,~]       = mon.dist2ACs(5,30);
+[cdistCte,~,~,closestACCte,~] = monCte.dist2ACs(5,30);
 
 AcelCte = zeros(1,monCte.lastTime);
 VverCte = zeros(1,monCte.lastTime);
@@ -29,11 +29,14 @@ Vver(mon.lastTime) = Vver(mon.lastTime-1);
 
 %% Array de tiempos en que se cambia de WP
 % VELOCIDAD PROGRESIVA logAC1.90s.velProg.csv
-waypoints = [236 550 684 735 910 1177 1223 1502 1555 1608 1634 1915];
-
+% waypoints = [236 550 684 735 910 1177 1223 1502 1555 1608 1634 1915];
 % VELOCIDAD CONSTANTE logAC1.90s.velCte.csv
-waypointsCte = [236 550 684 747 923 1209 1513 1553 1591 1651 1952];
+% waypointsCte = [236 550 684 747 923 1209 1513 1553 1591 1651 1952];
 
+% VELOCIDAD PROGRESIVA logAC5.90s.velProg.csv
+waypoints = [596 910 1044 1095 1270 1537 1584 1778 1832 1888 2092 2153];
+% VELOCIDAD CONSTANTE logAC5.90s.velCte.csv
+waypointsCte = [596 910 1044 1107 1283 1569 1800 1838 1876 2131 2247];
 %% Creamos la figura
 figHandler = findobj('Type','figure','Name','AC1')';
 if isempty(figHandler)
@@ -55,15 +58,15 @@ ax0 = nexttile;
 
 yyaxis right
 ax0.YAxis(2).Color = 'black';
-axis([800 2000  0 convlength(2500,'m','ft')])
+axis([waypoints(3)-5 waypoints(end)  0 convlength(2500,'m','ft')])
 ylabel('altitude (ft)') 
 
 yyaxis left
 ax0.YAxis(1).Color = 'black';
-axis([800 2000  0 2500])
+axis([waypoints(3)-5 waypoints(end)  0 2000])
 ylabel('altitude (m)')
 hold on
-
+grid on
 plot(1:monCte.lastTime,posZCte,'-r','LineWidth',1)
 plot(1:mon.lastTime,posZ, '-' ,'LineWidth',1)
 
@@ -78,15 +81,15 @@ ax1 = nexttile;
 
 yyaxis right
 ax1.YAxis(2).Color = 'black';
-axis([800 2000  convvel(-30,'m/s','kts') convvel(30,'m/s','kts')])
+axis([waypoints(3)-5 waypoints(end)  convvel(-30,'m/s','kts') convvel(30,'m/s','kts')])
 ylabel('Vertical speed (kt)') 
 
 yyaxis left
 ax1.YAxis(1).Color = 'black';
-axis([800 2000  -30 30])
+axis([waypoints(3)-5 waypoints(end)  -10 10])
 ylabel('Vertical speed (m/s)')
 hold on
-
+grid on
 plot(1:monCte.lastTime,VverCte,'-r','LineWidth',1)
 plot(1:mon.lastTime,Vver, '-' ,'LineWidth',1)
 
@@ -102,15 +105,15 @@ ax2 = nexttile;
 
 yyaxis right
 ax2.YAxis(2).Color = 'black';
-axis([800 2000  convang(-12,'rad','deg') convang(-2,'rad','deg')])
+axis([waypoints(3)-5 waypoints(end)  convang(-12,'rad','deg') convang(-2,'rad','deg')])
 ylabel('heading (deg)') 
 
 yyaxis left
 ax2.YAxis(1).Color = 'black';
-axis([800 2000  -12 -2])
+axis([waypoints(3)-5 waypoints(end)  -12 -2])
 ylabel('heading (rad)')
 hold on
-
+grid on
 plot(1:monCte.lastTime,psiCte,'-r','LineWidth',1)
 plot(1:mon.lastTime,psi, '-' ,'LineWidth',1)
 
@@ -125,15 +128,15 @@ ax3 = nexttile;
 
 yyaxis right
 ax3.YAxis(2).Color = 'black';
-axis([800 2000  0 convvel(140,'m/s','kts')])
+axis([waypoints(3)-5 waypoints(end)  0 convvel(140,'m/s','kts')])
 ylabel('forward speed (kt)') 
 
 yyaxis left
 ax3.YAxis(1).Color = 'black';
-axis([800 2000  0 120])
+axis([waypoints(3)-5 waypoints(end)  0 140])
 ylabel('forward speed (m/s)')
 hold on
-
+grid on
 plot(1:monCte.lastTime,VhorCte,'-r','LineWidth',1)
 plot(1:mon.lastTime,Vhor, '-' ,'LineWidth',1)
 
@@ -149,16 +152,16 @@ ax4 = nexttile;
 
 yyaxis right
 ax4.YAxis(2).Color = 'black';
-axis([800 2000  -10/9.8 10/9.8])
+axis([waypoints(3)-5 waypoints(end)  -20/9.8 20/9.8])
 ylabel('acceleration (G)')
 
 yyaxis left
 ax4.YAxis(1).Color = 'black';
-axis([800 2000  -10 10])
+axis([waypoints(3)-5 waypoints(end)  -20 20])
 ylabel('acceleration (m/s^{2})');
 
 hold on
-
+grid on
 plot(1:monCte.lastTime,AcelCte,'-r','LineWidth',1)
 plot(1:mon.lastTime,Acel, '-','Color','#0072BD' ,'LineWidth',1)
 
@@ -175,16 +178,16 @@ ax5 = nexttile;
 
 yyaxis right
 ax5.YAxis(2).Color = 'black';
-axis([800 2000  0 0.161987])
+axis([waypoints(3)-5 waypoints(end)  0 200*0.0005399568])
 ylabel('tracking error (NM)')
 
 yyaxis left
 ax5.YAxis(1).Color = 'black';
-axis([800 2000  0 300])
+axis([waypoints(3)-5 waypoints(end)  0 200])
 ylabel('tracking error (m)')
 
 hold on
-
+grid on
 plot(1:monCte.lastTime,distCte,'-r' ,'LineWidth',1)
 % disp("Area tracking error cte: ");
 % disp( trapz (1209:1591 , distCte(1209:1591)));
@@ -205,16 +208,16 @@ ax6 = nexttile;
 
 yyaxis right
 ax6.YAxis(2).Color = 'black';
-axis([800 2000  convlength(4000,'m','naut mi') convlength(12000,'m','naut mi')])
+axis([waypoints(3)-5 waypoints(end)  convlength(4000,'m','naut mi') convlength(12000,'m','naut mi')])
 ylabel('distance (NM)') 
 
 xlabel('time (s)')
 yyaxis left
 ax6.YAxis(1).Color = 'black';
-axis([800 2000  4000 12000])
+axis([waypoints(3)-5 waypoints(end)  4000 12000])
 ylabel('distance (m)')
 hold on
-
+grid on
 plot([1 mon.lastTime],[5556 5556],'-.','LineWidth',1,'Color',[0.5 0.5 0.5]);
 plot(1:monCte.lastTime,cdistCte,'-r' ,'LineWidth',1)
 plot(1:mon.lastTime,cdist,'-' ,'LineWidth',1)
